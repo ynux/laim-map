f594e34541204484a73c4390025beb261c52a63d
mitmirzutun <milan1111@gmx.de>
2020-03-08 13:13:53 +0100

    Add files via upload

---
+++ Unfallorte_Formatierung.py
@@ -0,0 +1,34 @@
+import pyproj
+P = pyproj.Proj(proj='utm', zone="32N", ellps='WGS84', preserve_units=True)
+def XY_To_LatLon(x,y):
+    return P(x,y,inverse=True)
+with open("init.txt","r") as init:
+    for zeile in init:
+        while zeile[-1]=="\r" or zeile[-1]=="\n":zeile=zeile[:-1]
+        print(zeile)
+        try:
+            with open("Unfallorte"+zeile+"_LinRef.txt","r") as data_in:
+                with open("Unfallorte"+zeile+"_LinRef.csv","w") as data_out:
+                    line = data_in.readline()
+                    n=1
+                    for c in line[:-1]:
+                        if c==";":n+=1
+                        data_out.write(c)
+                        continue
+                    data_out.write(";lat;lon\n")
+                    for line in data_in:
+                        l=["",""]
+                        n1=n
+                        for c in line:
+                            if c=="\n": continue
+                            if c==";": n1-=1
+                            elif n1<=2:
+                                if c==",": c="."
+                                l[2-n1]+=c
+                            data_out.write(c)
+                        l[0]=float(l[0])
+                        l[1]=float(l[1])
+                        l=list(XY_To_LatLon(l[0],l[1]))
+                        data_out.write(";"+str(l[0])+"E;"+str(l[1])+"N\n")
+        except FileNotFoundError:
+            print("Unfallorte"+zeile+"_LinRef.txt existiert nicht")
